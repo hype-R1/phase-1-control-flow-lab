1f293e55a4da456557ad4d5ac38b928a56343b73
Michael <mjpj@live.com>
2021-11-06 18:53:06 -0400

    commit tree 619f7e7d38055a792779cb3dd9137227798e0393

--- index.js
+++ index.js
@@ -1,24 +1,117 @@
-function scuberGreetingForFeet (feet) {
-    if (feet <= 400) {
-      return 'This one is on me!';
-    } else if (1999 < feet && feet < 2500) {
-      return "I will gladly take your thirty bucks.";
-    } else {
-      return "No can do.";
-    }
-  }
+
+ function scuberGreetingForFeet(feet){
+   if (feet < 400 ){
+     return 'This one is on me!';
+   }else if (feet > 1999 && feet < 2500){
+     return 'I will gladly take your thirty bucks.';
+   }else {(feet > 2500)
+       return 'No can do.';
+     }
+   }
+
   
-  function ternaryCheckCity (city) {
-    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
-  }
-  
-  function switchOnCharmFromTip (tip) {
-    switch (tip) {
-      case 'generous':
-        return 'Thank you so much.';
-      case 'not as generous':
-        return 'Thank you.';
-      default:
-        return 'Bye.';
-    }
-  }
+     function ternaryCheckCity(parm) {
+       return parm === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+     }
+   
+ function switchOnCharmFromTip(tip) {
+   if (tip === 'generous') {
+     return 'Thank you so much.';
+   } else if  (tip === 'not as generous'){
+     return 'Thank you.';
+   } else {
+     return 'Bye.'
+   }
+ }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function scuberGreetingForFeet (feet) {
+//   if (feet <= 400) {
+//     return 'This one is on me!';
+//   } else if (1999 < feet && feet < 2500) {
+//     return "I will gladly take your thirty bucks.";
+//   } else {
+//     return "No can do.";
+//   }
+// }
+
+// function ternaryCheckCity (city) {
+//   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+// }
+
+// function switchOnCharmFromTip (tip) {
+//   switch (tip) {
+//     case 'generous':
+//       return 'Thank you so much.';
+//     case 'not as generous':
+//       return 'Thank you.';
+//     default:
+//       return 'Bye.';
+//   }
+// }
